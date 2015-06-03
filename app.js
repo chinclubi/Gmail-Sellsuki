73c1a3546edc538e35aa47b1786893987f260b31
ChinCluBi <chincub@gmail.com>
2015-06-04 01:08:38 +0700

    Get more message when scroll to bottom

--- app.js
+++ app.js
@@ -10,16 +10,22 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.get('/inbox/:token/', function(req, res){
+app.get('/inbox/:token/:n', function(req, res){
   var access_token = req.params.token;
-  var _url = 'https://www.googleapis.com/gmail/v1/users/me/messages?labelIds=INBOX&maxResults=10&q=category%3Aprimary&access_token='+access_token;
-  var objMsg = [];
+  var _url = 'https://www.googleapis.com/gmail/v1/users/me/messages?labelIds=INBOX&maxResults=20&q=category%3Aprimary&access_token='+access_token;
+  var nPage = req.params.n;
+  if(nPage != '-1'){
+    _url += '&pageToken='+nPage;
+  }
+  var objMsg = {list :[], nextPage:""};
   request(_url, function(error, response, body){
     var jsonResult = JSON.parse(body);
+    console.log(jsonResult);
     var msgList = jsonResult.messages;
     msgList.forEach(function(item){
-      objMsg.push(item.threadId);
+      objMsg.list.push(item.threadId);
     });
+    objMsg.nextPage = jsonResult.nextPageToken;
     res.json(objMsg);
   });
 });
