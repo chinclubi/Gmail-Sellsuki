bf91e13fabc77c675db7ef6b6e48d3b36676c8bd
ChinCluBi <chincub@gmail.com>
2015-06-06 02:01:38 +0700

    v0.1.3 Gmail Sellsuki - Clean Code

--- js/index.js
+++ js/index.js
@@ -1,76 +1,73 @@
-angular.module('GmailSellsuki', []).controller('appController', ['$scope', function($scope){
+angular.module('GmailSellsuki', []).controller('appController', ['$scope', 'showState', function($scope, showState){
 
-  $scope.client_id = '331691048436-q1g7qk6qf50hvg896regfa2pdv0n1q6h.apps.googleusercontent.com';
+  $scope.clientId = '331691048436-q1g7qk6qf50hvg896regfa2pdv0n1q6h.apps.googleusercontent.com';
   $scope.scopes = ['https://mail.google.com/'];
   $scope.nextPage = '';
-  $scope.access_token = '';
   $scope.emails = [];
-  $scope.state = {loginBtn:0, message:0, loadMore: 0};
+  $scope.state = showState.init();
 
   $scope.checkAuth = function checkAuth(immediate){
-    gapi.auth.authorize({client_id: $scope.client_id, scope: $scope.scopes, immediate: immediate}, $scope.result);
+    gapi.auth.authorize({
+      client_id: $scope.clientId,
+      scope: $scope.scopes,
+      immediate: immediate
+    }, $scope.authResult);
   }
 
-  $scope.result = function result(res){
+  $scope.authResult = function(res){
     if(res && !res.error){
-      $scope.access_token = res.access_token;
-      $scope.createInbox();
-      $scope.state.message = 1;
-      $scope.state.loginBtn = 0;
-      $scope.state.loadMore = 1;
+      $scope.$apply(function(){
+        $scope.state = showState.normalState();
+        $scope.createInbox();
+      });
     }else{
-      $scope.state.message = 0;
-      $scope.state.loginBtn = 1;
-      $scope.state.loadMore = 0;
+      $scope.$apply(function(){
+        $scope.state = showState.loginState();
+      });
     }
-    $scope.$apply();
   }
 
-  $scope.createInbox = function createInbox(){
+  $scope.createInbox = function(){
+    $scope.state = showState.loadState();
+    $scope.findMessageList( function(result){
 
-    $("#loadBtn").button('loading');
-
-    $scope.getMessageList(function(result){
-      result.messages.forEach(function(item){
-        $scope.getMessage(item.threadId, function(res){
-          var tmp = {}, n = 0, header = res.payload.headers;
-          for(var i=0; i < header.length ; i++){
-            if(header[i].name == 'Subject'){
-              tmp.Subject = header[i].value;
-              n++;
-            }
-            if(header[i].name == 'From'){
-              tmp.From = header[i].value;
-              n++;
-            }
-            if(n==2) break;
-          }
-          $scope.emails.push(tmp);
-          $scope.$apply();
-          $('#loadBtn').button('reset');
-        });
+      angular.forEach(result.messages, function(value, key){
+        $scope.getMessage(value.threadId, $scope.findMessageDetail);
       });
 
       $scope.nextPage = result.nextPageToken;
+
       if(typeof $scope.nextPage == 'undefined'){
-        $scope.state.loadMore = 0;
-        $scope.$apply();
+        $scope.$apply(function(){
+          $scope.state = showState.noEnoughState();
+        });
       }
 
     });
   }
 
-  $scope.getMessage = function getMessage(messageId, callback) {
-    gapi.client.load('gmail', 'v1').then(function(){
-      var request = gapi.client.gmail.users.messages.get({
-        'userId': 'me',
-        'id': messageId
-      });
-      request.execute(callback);
+  $scope.findMessageDetail = function(res){
+    var tmp = {}, n = 0, header = res.payload.headers;
+    for(var i=0; i < header.length ; i++){
+      if(header[i].name == 'Subject'){
+        tmp.Subject = header[i].value;
+        n++;
+      }
+      if(header[i].name == 'From'){
+        tmp.From = header[i].value;
+        n++;
+      }
+      if(n==2) break;
+    }
+
+    $scope.$apply(function(){
+      $scope.emails.push(tmp);
+      $scope.state = showState.normalState();
     });
   }
 
-  $scope.getMessageList = function getMessageList(callback){
+  $scope.findMessageList = function(callback){
+
     if (typeof $scope.nextPage != 'undefined'){
       gapi.client.load('gmail', 'v1').then(function(){
         var request = gapi.client.gmail.users.messages.list({
@@ -82,5 +79,42 @@ angular.module('GmailSellsuki', []).controller('appController', ['$scope', funct
         request.execute(callback);
       });
     }
+
   }
-}]);
+
+  $scope.getMessage = function(messageId, callback) {
+    gapi.client.load('gmail', 'v1').then(function(){
+      var request = gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': messageId
+      });
+      request.execute(callback);
+    });
+  }
+
+}]).factory('showState', function(){
+  var init = function(){
+    return { loginBtn: 0, message: 0, loadMore: 0, loading: [0, 'Load More'] };
+  }
+  var loginState = function(){
+    return { loginBtn: 1, message: 0, loadMore: 0, loading: [0, 'Load More'] };
+  }
+  var normalState = function(){
+    return { loginBtn: 0, message: 1, loadMore: 1, loading: [0, 'Load More'] };
+  }
+  var noEnoughState = function(){
+    return { loginBtn: 0, message: 1, loadMore: 0, loading: [0, 'Load More'] };
+  }
+  var loadState = function(){
+    return { loginBtn: 0, message: 1, loadMore: 1, loading: [1, 'Loading..'] };
+  }
+
+  return {
+    init : init,
+    loginState : loginState,
+    normalState : normalState,
+    noEnoughState : noEnoughState,
+    loadState : loadState
+  }
+
+});
